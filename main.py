cc2046e93adf48a9a1d1298cda540f4b2cfccaa6
Grouli123 <S00yer@mail.ru>
2023-09-17 02:38:35 +0300

    add main bat file to start 3 scripts

---
+++ main.py
@@ -0,0 +1,20 @@
+import subprocess
+
+# Список путей к скриптам, которые нужно запустить
+script_paths = [
+    'admin_main.py',
+    'get_orders_main.py',
+    'registration_main.py'
+]
+
+# Список для хранения процессов
+processes = []
+
+# Запускаем каждый скрипт в отдельном процессе
+for script_path in script_paths:
+    process = subprocess.Popen(['python', script_path])
+    processes.append(process)
+
+# Дожидаемся завершения всех процессов
+for process in processes:
+    process.wait()
